c5a9c12caf825a5fabdef34c63757eb909934eca
fanyoufu <fanyoufu@outlook.com>
2020-08-10 14:55:31 +0800

    401处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,6 +11,7 @@ import store from '@/store/index.js'
 
 import JSONBig from 'json-bigint' // 引入大数字包
 
+import router from '@/router/index.js'
 const instance1 = axios.create({
   // 后端服务器1
   baseURL: 'http://ttapi.research.itcast.cn',
@@ -43,7 +44,56 @@ instance1.interceptors.request.use(function (config) {
 }, function (error) {
   // 对请求错误做些什么
   return Promise.reject(error);
-});
+})
+
+// 添加一个响应拦截器 -- 所有的请求在发回来之后，都会到这里来
+instance1.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, async function (error) {
+  console.log('响应拦截器，拦截到了错误')
+  if (error.response && error.response.status === 401) {
+    // 2.遇到了401错误
+    const refreshToken = store.state.tokenInfo.refresh_token
+    if (refreshToken) {
+      // 3. 用resfreshToken再发请求
+      try {
+        const result = await axios({
+          method: 'PUT',
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${refreshToken}`
+          }
+        })
+        // 4. 成功获取新token
+        console.log('用refresh_token取回来的新的token', result)
+        const newToken = result.data.data.token
+        // 5. 保存新token到vuex
+        store.commit('mSetTokenInfo', {
+          token: newToken,
+          refresh_token: refreshToken
+        })
+
+        // 6. 重新发请求
+        // error.表示401错误。error.config表示出这个错误时，所要发出的请求的配置
+        return instance1(error.config) // 重新去发一次请求
+      } catch {
+        console.log('用refresh_token更新token失败, 只能去做一次登陆')
+        // 路由跳转
+        // currentRoute: 当前路由
+        router.push('/login?backto=' + router.currentRoute.fullPath)
+      }
+    } else {
+      // 回到登陆页
+      console.log('没有refresh_token, 只能去做一次登陆')
+      // 路由跳转
+      // currentRoute: 当前路由
+      router.push('/login?backto=' + router.currentRoute.fullPath)
+    }
+  }
+  return Promise.reject(error);
+})
 
 const instance2 = axios.create({
   // 后端服务器2
