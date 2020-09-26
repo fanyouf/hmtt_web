e64fd1ee71c633b729e7a6e2b1019dad5600ea1f
fanyoufu <fanyoufu@outlook.com>
2020-09-26 15:42:59 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <div id="nav">
       黑马头条web版本
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
     </div>
     <router-view/>
   </div>
