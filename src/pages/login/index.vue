a0a70d776df30c3b93d60d85287f70afe5d0a605
fanyoufu <fanyoufu@outlook.com>
2020-09-27 09:52:13 +0800

    登陆核心功能

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -8,36 +8,63 @@
     <!-- 2. 输入区域 -->
     <van-cell-group>
       <van-field
-        v-model="userInfo.phone"
+        v-model.trim="userInfo.mobile"
         required
         label="手机号"
         placeholder="请输入手机号"
         error-message=""
       />
       <van-field
-        v-model="userInfo.code"
+        v-model.trim="userInfo.code"
         required
-        label="密码"
+        label="短信验证"
         placeholder="请输入密码"
       />
     </van-cell-group>
 
     <!-- 3. 按钮 -->
     <div class="btn-wrap">
-      <van-button round type="info" class="btn">登录</van-button>
+      <van-button round type="info" class="btn" @click="hLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from '@/utils/request.js'
 export default {
   name: 'login',
   data () {
     return {
       userInfo: {
-        phone: '13912345678',
+        mobile: '13912345678',
         code: '246810'
       }
+    }
+  },
+  methods: {
+    async hLogin () {
+      // 1. 表单验证
+      // TODO: 表单验证
+      console.log(this.userInfo)
+      // 2. 发请求
+      // (1) 引入axios， （2）传入接口所需的参数
+      const { mobile, code } = this.userInfo
+      try {
+        const result = await axios({
+          url: '/app/v1_0/authorizations',
+          method: 'POST',
+          data: {
+            mobile,
+            code
+          }
+        })
+        console.log(result)
+        alert('成功')
+      } catch (err) {
+        console.log(err)
+        alert('登陆失败')
+      }
+      // 3. 根据请求结果给出反馈，做后续
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -9,14 +9,14 @@ import axios from 'axios'
 // axios.defaults.baseURL = '' // 基地址
 const instance1 = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn', // 后端小张同学写的
-  timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  timeout: 1000
+  // headers: { 'X-Custom-Header': 'foobar' }
 })
 
 const instance2 = axios.create({
   baseURL: 'http://api-toutiao-web.itheima.net', // 后端老王同学写的
-  timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  timeout: 1000
+  // headers: { 'X-Custom-Header': 'foobar' }
 })
 
 export {
